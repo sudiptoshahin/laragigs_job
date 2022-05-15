f42806ef53a5321c010b031e68362f0a62f21aa4
Sudipto Shahin <sudipto.shine@gmail.com>
2022-05-15 22:54:20 +0600

    working on template

---
+++ resources/views/listing.blade.php
@@ -0,0 +1,5 @@
+
+
+<h2>{{ $list['title'] }}</h2>
+
+<p>{{ $list['description'] }}</p>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Listing;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading'=> 'Latest Listing',
+        'listing'=> Listing::all(),
+    ]);
 });
+
+
+Route::get('/listing/{id}', function($id){
+
+    return view('listing', [
+        'list'=> Listing::find($id),
+    ]);
+})->name('listing');
+
